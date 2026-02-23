2625be7c2a697407365a263d52f090ae8ea221dd
Adarsh165787 <adarshkumr1657@gmail.com>
2026-02-23 19:23:21 +0530

    Added new folder

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,15 @@
+import Chai from "./Raj";
+
+
+
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>ram naam ki jay</h1>
+    </>
+  );
+}
+export default App;
+// export default Chai;
+
---
+++ 01basicreact/src/Raj.js
@@ -0,0 +1,10 @@
+function Chai(){
+    return (
+        <>
+        <h1>Radhe Radhe</h1>
+        <h2>Hello there</h2>
+        <h3>xxxx</h3>
+        </> 
+    )
+}
+export default Chai;
